b4529ba07744bc67783018c7f7ffb4be1a713efe
Dammir2805 <d_kalymbaev@mail.ru>
2019-12-18 11:12:52 +0300

    Initial commit

---
+++ urlshort.rb
@@ -0,0 +1,14 @@
+
+# First of all I found and installed gem 'corto'
+# Then I installed this gem and configuered one
+require "corto"
+corto = Corto.new
+
+# Now we ask user to input url
+puts "Введите URL"
+url = STDIN.gets.chomp
+
+puts url
+
+
+
